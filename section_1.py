797e091db124b1a2ef49c77a5e3ad1afa5d9b3d2
quintanillagreg2 <grmquint@ucsc.edu>
2026-04-06 21:59:58 -0700

    Update section_1.py

--- section_1.py
+++ section_1.py
@@ -1,3 +1,4 @@
 print("Hello, my name is Gregory Quintanilla (he/him).")
 print("My favorite movie is the Revenant, directed by Alejandro Innaritu.")
 print("My favorite food is chicken shawrma, I love mediterranean food.")
+#from this assignment I learned how to more effectively upload files to GitHub. I learned how to use repositories and edit file uploads. 
