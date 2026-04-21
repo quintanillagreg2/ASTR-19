f3c50c0819d0f71e1aa3a3f0c044d8185386ad22
quintanillagreg2 <grmquint@ucsc.edu>
2026-04-21 10:57:00 -0700

    Add files via upload

---
+++ J1_T2.py
@@ -0,0 +1,12 @@
+x=3.55
+y=4.21
+print(x+y)
+print("class datatype is", type(x+y).__name__) #.__name__ separates the main word
+a=5
+b=2
+print(a-b)
+print("class datatype is", type(a+b).__name__)
+z=22.7
+w=-610.8
+print(z*w)
+print("the datatype is", type(z*w).__name__)
