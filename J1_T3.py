ac664013e3454adade0e078e27b183cd8e8e23c8
quintanillagreg2 <grmquint@ucsc.edu>
2026-04-21 11:11:11 -0700

    Add files via upload

---
+++ J1_T3.py
@@ -0,0 +1,10 @@
+def f(x):
+    return x**3+8
+def main():
+    x=9
+    result = f(x)
+    print("The final answer is", result)
+    if result > 27:
+        print("YAY!!!!")
+if __name__ == "__main__":
+    main()
